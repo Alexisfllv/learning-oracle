063d1096446e442d69f81c9ba69f7dab9c069588
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2025-10-23 10:16:40 -0500

    COMENTARIOS

---
+++ plsql010.sql
@@ -0,0 +1,12 @@
+-- Comentarios
+
+
+-- Este es un comentario de una sola línea
+SELECT * FROM empleados; -- También puede ir al final de la línea
+
+
+/*
+   Esto es un comentario
+   de varias líneas en SQL Developer
+*/
+SELECT * FROM departamentos;
